48a56d0f74e82fa7334f177fb2c5cd8f8c5196a8
謝鎮宇 <xiezhenyu@xiezhenyus-MacBook-Pro.local>
2017-01-16 01:18:37 +0800

    apply rxlifecycle2

--- app/src/main/java/com/silver/cat/nilo/MainActivity.java
+++ app/src/main/java/com/silver/cat/nilo/MainActivity.java
@@ -1,9 +1,10 @@
 package com.silver.cat.nilo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+
+public class MainActivity extends RxAppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
