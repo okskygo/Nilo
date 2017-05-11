bb1b79ff56698ca52047c0c78d1d1ffa640228e6
Rex <okskygo@gmail.com>
2017-05-11 15:07:36 +0800

    apply base activity and fragment

---
+++ app/src/main/java/com/silver/cat/nilo/view/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.silver.cat.nilo.view
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
+
+/**
+ * Created by xiezhenyu on 2017/5/11.
+ */
+
+abstract class BaseActivity : RxAppCompatActivity() {
+    //maybe future useful
+}
---
+++ app/src/main/java/com/silver/cat/nilo/view/BaseFragment.kt
@@ -0,0 +1,11 @@
+package com.silver.cat.nilo.view
+
+import com.trello.rxlifecycle2.components.support.RxFragment
+
+/**
+ * Created by xiezhenyu on 2017/5/11.
+ */
+
+abstract class BaseFragment : RxFragment() {
+    //maybe future useful
+}
