298c7e757d3b69f1dd08d4205e248ab2a3ea9c58
Rex <okskygo@gmail.com>
2017-05-11 15:08:41 +0800

    apply main bottom navigation with viewpager

--- app/src/main/java/com/silver/cat/nilo/view/main/MainActivity.kt
+++ app/src/main/java/com/silver/cat/nilo/view/main/MainActivity.kt
@@ -1,12 +1,98 @@
 package com.silver.cat.nilo.view.main
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.view.MenuItem
+import com.silver.cat.nilo.R
+import com.silver.cat.nilo.view.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 
-class MainActivity : RxAppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewPager()
+        setupBottomNavigationView()
     }
+
+    private fun setupViewPager() {
+        with(viewPager) {
+            adapter = MainViewPagerAdapter(supportFragmentManager)
+            addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+                var prevMenuItem: MenuItem? = null
+
+                override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+                }
+
+                override fun onPageSelected(position: Int) {
+                    if (prevMenuItem != null) {
+                        prevMenuItem?.isChecked = false
+                    } else {
+                        bottomNavigationView.menu.getItem(0).isChecked = false
+                    }
+
+                    bottomNavigationView.menu.getItem(position).isChecked = true
+                    prevMenuItem = bottomNavigationView.menu.getItem(position)
+                }
+
+                override fun onPageScrollStateChanged(state: Int) {
+
+                }
+            })
+        }
+    }
+
+    private fun setupBottomNavigationView(){
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+
+            when (item.itemId) {
+                R.id.main_bottom_navigation_friend_list -> {
+                    viewPager.setCurrentItem(MainViewFragmentEnum.FRIEND_LIST.ordinal, true)
+                }
+                R.id.main_bottom_navigation_chat -> {
+                    viewPager.setCurrentItem(MainViewFragmentEnum.CHAT.ordinal, true)
+
+                }
+                R.id.main_bottom_navigation_setting -> {
+                    viewPager.setCurrentItem(MainViewFragmentEnum.SETTING.ordinal, true)
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+    }
+
+}
+
+enum class MainViewFragmentEnum {
+    FRIEND_LIST, CHAT, SETTING;
+
+    companion object {
+        fun getFragment(enum: MainViewFragmentEnum): Fragment =
+                when (enum) {
+                    FRIEND_LIST -> FriendListFragment()
+                    CHAT -> ChatFragment()
+                    SETTING -> SettingFragment()
+                }
+    }
+
+
+}
+
+class MainViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+    override fun getCount(): Int {
+        return MainViewFragmentEnum.values().size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return MainViewFragmentEnum.getFragment(MainViewFragmentEnum.values()[position])
+    }
+
 }
---
+++ app/src/main/java/com/silver/cat/nilo/view/main/SettingFragment.kt
@@ -0,0 +1,27 @@
+package com.silver.cat.nilo.view.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.silver.cat.nilo.R
+import com.silver.cat.nilo.view.BaseFragment
+
+/**
+ * Created by xiezhenyu on 2017/5/11.
+ */
+
+class SettingFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
